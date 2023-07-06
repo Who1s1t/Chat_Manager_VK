69beee339e6d5e3fd24f19ea8bb2298f98b0c1be
OVER <dedsek965@gmail.com>
2023-07-06 17:56:03 +0300

    edit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-for(let i = 1n; i <=  2n**256n; i += 1n){
+import VkBot from "node-vk-bot-api";
 
-}
-console.log(ffff)
+const bot = new VkBot({
+    token: 'vk1.a.fX9M1GCRQp-02uFilQUwS5c9pm2VLSxGgxBcizKo3yDKaiBjy3zt-UbCo8fHUx6xlTWUlyPoBHBa3klzvMrUl_2NozA4hGMHO3hW5EXbVS6BYvO7HjE1G72LzoBCnhDWFl8Xw-qsTeVi5HCA92fj2vFDR2d6y9ruT1dIsGm28hHm7CN-qSzp0lde0z4XpWEQszJbYs55kAJn7AZq0NRPiw',
+
+    group_id: 221221699,
+
+});
