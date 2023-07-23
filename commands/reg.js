fab5c06a059450c4c1625ebaac3742424c585fa6
OVER <dedsek965@gmail.com>
2023-07-23 17:30:48 +0300

    v 0.1.2

--- commands/reg.js
+++ commands/reg.js
@@ -2,21 +2,26 @@ const Users = require('../db/users.js')
 
 
 module.exports = async function (ctx, bot) {
-    const sender = await Users.findOne({
+    const vk_id = (ctx.message.action && ctx.message.action.type === 'chat_invite_user') ? ctx.message.action.member_id : ctx.message.from_id;
+    const user = await Users.findOne({
         where : {
-            vk_id : ctx.message.from_id
+            vk_id
         }
     });
-    if (sender) return;
-    const user = await bot.execute('users.get',{
-        user_id :  ctx.message.from_id
+
+    if (user) return;
+
+    const user_vk = await bot.execute('users.get',{
+        user_id :  vk_id
     });
+
     await Users.create({
-        vk_id: user[0].id,
-        firstname: user[0].first_name,
-        lastname: user[0].last_name,
+        vk_id: user_vk[0].id,
+        firstname: user_vk[0].first_name,
+        lastname: user_vk[0].last_name,
         rate: 0,
-        role: 1,
+        role: 0,
     })
-    await ctx.reply(`${user[0].first_name} зарегистрирован`);
+
+    await ctx.reply(`${user_vk[0].first_name} зарегистрирован`);
 };
